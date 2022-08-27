aee4f58055b3617f0011cc22022d7dc3c467eedf
dori-dev <mr.dori.dev@gmail.com>
2022-08-27 15:17:47 +0430

    create column function

---
+++ orm.py
@@ -0,0 +1,14 @@
+def column(name: str, type: str,
+           primary_key: bool = False, unique: bool = False,
+           nullable: bool = True, default: str = None):
+    if primary_key:
+        return f'{name} {type} PRIMARY KEY NOT NULL'
+    constraints = ''
+    if nullable:
+        if default is not None:
+            constraints += f'DEFAULT {default}'
+    else:
+        constraints += 'NOT NULL'
+    if unique:
+        constraints += 'UNIQUE'
+    return f'{name} {type} {constraints}'
