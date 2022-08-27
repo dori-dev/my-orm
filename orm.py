8226de47e7a9164656dc65731814344eaeef5410
dori-dev <mr.dori.dev@gmail.com>
2022-08-27 16:50:04 +0430

    add execute query function

--- orm.py
+++ orm.py
@@ -1,3 +1,8 @@
+import sqlite3
+
+DB_NAME = 'test.db'
+
+
 def column(name: str, type: str,
            primary_key: bool = False, unique: bool = False,
            nullable: bool = True, default: str = None):
@@ -12,3 +17,8 @@ def column(name: str, type: str,
     if unique:
         constraints += 'UNIQUE'
     return f'{name} {type} {constraints}'.strip()
+
+
+def execute_query(query: str):
+    conn = sqlite3.connect(DB_NAME)
+    conn.execute(query)
