28593a65a4485941aaa04dc7ec5982a5425eb22a
dori-dev <mr.dori.dev@gmail.com>
2022-08-28 11:42:09 +0430

    add all method for get all data

--- orm.py
+++ orm.py
@@ -27,6 +27,19 @@ class GetColumns:
         return result
 
 
+class Row:
+    def __init__(self, **data):
+        for key, value in data.items():
+            self.__setattr__(key, value)
+
+    def __repr__(self) -> str:
+        data = self.__dict__
+        result = ' | '.join([
+            f'{repr(attr)}:{repr(data[attr])}' for attr in data
+        ])
+        return f"<{result}>"
+
+
 class DB:
     db_name = GenerateDBName()
     table_name = GenerateTableName()
@@ -59,11 +72,30 @@ class DB:
         self._execute(query)
 
     @classmethod
+    def all(cls):
+        query = f'SELECT * FROM {cls.table_name}'
+        result = []
+        rows = cls._fetchall(query)
+        for row in rows:
+            row = dict(zip(cls.columns.keys(), row))
+            result.append(Row(**row))
+        return result
+
+    @classmethod
     def _execute(cls, query: str):
         conn = sqlite3.connect(cls.db_name)
         conn.execute(query)
         conn.commit()
         conn.close()
+
+    @classmethod
+    def _fetchall(cls, query: str) -> list:
+        conn = sqlite3.connect(cls.db_name)
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+        conn.close()
+        return rows
 
 
 def column(type: str, primary_key: bool = False, unique: bool = False,
